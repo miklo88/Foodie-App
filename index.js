6335082d500cf59f154e4f39a29200a265acb032
Carlos <miklo.concepcion@gmail.com>
2020-01-29 21:14:35 -0600

    api is up and running on https://localhost:9000 still going throught and cleaning enviornment. working on finishing up authentication jwt token and auth router to see if i'm creating/passing a user token

--- index.js
+++ index.js
@@ -4,13 +4,8 @@ require("dotenv").config();
 
 const server = require("./api/server");
 
-// localhost from .env file
-const host = process.env.HOST;
-// port 9000 front .env file
-const port = process.env.PORT;
-// IF YOU FORKED THIS FILE AND WANT TO RUN THE SERVER COMMENT OUT CODE ABOVE AND UNCOMMENT THESE NEXT TWO LINES
-// const host = process.env.HOST || "localhost";
-// const port = process.env.PORT || 9000;
+const host = process.env.HOST || "localhost";
+const port = process.env.PORT || 9000;
 
 // backend server port/host.
 server.listen(port, () => {
