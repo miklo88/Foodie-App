b1ebeb9a6e35e0e326ea73dce372e46975f1e645
Carlos <miklo.concepcion@gmail.com>
2020-01-31 00:10:10 -0600

    users seed debugging.

--- database/seeds/users-001.js
+++ database/seeds/users-001.js
@@ -1,13 +1,34 @@
-
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
+  return knex("users")
+    .del()
+    .then(function() {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex("users").insert([
+        {
+          id: 1,
+          firstName: "michelle",
+          lastName: "obama",
+          email: "email@gmail.com",
+          username: "username1",
+          password: "testpassword"
+        },
+        {
+          id: 2,
+          firstName: "paul",
+          lastName: "simon",
+          email: "email@gmail.com",
+          username: "username2",
+          password: "testpassword"
+        },
+        {
+          id: 3,
+          firstName: "leonardo",
+          lastName: "davinci",
+          email: "email@gmail.com",
+          username: "username3",
+          password: "testpassword"
+        }
       ]);
     });
 };
