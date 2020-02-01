7a5aa7fb08d420b5efb7fca50716569465a14ebc
Carlos <miklo.concepcion@gmail.com>
2020-02-01 13:52:05 -0600

    we re-rollin rollin rollin rollin. some reason sqlite can't find my email column in my table of users. interesante.

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -1,7 +1,8 @@
 const knex = require("knex");
 
-const knexConfig = require("../knexfile.js");
+const config = require("../knexfile.js");
 
-const dbEnv = process.env.DB_ENV || "development";
+// const dbEnv = process.env.DB_ENV || "development";
+const db = knex(config.development);
 
-module.exports = knex(knexConfig[dbEnv]);
+module.exports = db;
--- users/users-model.js
+++ users/users-model.js
@@ -1,40 +1,40 @@
-const bcrypt = require("bcryptjs");
-const db = require("../database/dbConfig");
+// const bcrypt = require("bcryptjs");
+// const db = require("../database/dbConfig");
 
-function find() {
-  return db("users").select("id", "firstName", "lastName");
-}
+// function find() {
+//   return db("users").select("id", "firstName", "lastName");
+// }
 
-function findBy(filter) {
-  return db("users")
-    .where(filter)
-    .select("id", "firstName", "lastName", "email", "password");
-}
+// function findBy(filter) {
+//   return db("users")
+//     .where(filter)
+//     .select("id", "firstName", "lastName", "email", "password");
+// }
 
-function findById(id) {
-  return db("users")
-    .where({ id })
-    .first("id", "email");
-}
+// function findById(id) {
+//   return db("users")
+//     .where({ id })
+//     .first("id", "email");
+// }
 
-async function add(user) {
-  user.password = await bcrypt.hash(user.password, 14);
+// async function add(user) {
+//   user.password = await bcrypt.hash(user.password, 14);
 
-  const [id] = await db("users").insert(user);
+//   const [id] = await db("users").insert(user);
 
-  return findById(id);
-}
+//   return findById(id);
+// }
 
-function remove(id) {
-  return db("users")
-    .where({ id })
-    .del();
-}
+// function remove(id) {
+//   return db("users")
+//     .where({ id })
+//     .del();
+// }
 
-module.exports = {
-  find,
-  findBy,
-  findById,
-  add,
-  remove
-};
+// module.exports = {
+//   find,
+//   findBy,
+//   findById,
+//   add,
+//   remove
+// };
