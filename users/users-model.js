c8934dfa6a8237b7efd4b974f7426ca81d1dd851
Carlos <miklo.concepcion@gmail.com>
2020-02-01 22:12:05 -0600

    stepping away from seeding and building users-router to pull users from db. users-model completed. building endpoint to test in insomnia

--- users/users-model.js
+++ users/users-model.js
@@ -1,40 +1,43 @@
 // const bcrypt = require("bcryptjs");
-// const db = require("../database/dbConfig");
+const db = require("../database/dbConfig");
 
-// function find() {
-//   return db("users").select("id", "firstName", "lastName");
-// }
+function find() {
+  return db("users");
+}
 
-// function findBy(filter) {
-//   return db("users")
-//     .where(filter)
-//     .select("id", "firstName", "lastName", "email", "password");
-// }
+function findBy(filter) {
+  return db("users")
+    .where(filter)
+    .select("id", "firstName", "lastName", "email", "password");
+}
 
-// function findById(id) {
-//   return db("users")
-//     .where({ id })
-//     .first("id", "email");
-// }
+function findById(id) {
+  return db("users")
+    .where({ id })
+    .first("id");
+}
 
-// async function add(user) {
-//   user.password = await bcrypt.hash(user.password, 14);
-
+function add(user) {
+  db("users")
+    .insert(user)
+    .then(ids => {
+      return findById(ids[0]);
+    });
+}
+//tuck in at line25 before the }   user.password = await bcrypt.hash(user.password, 14);
 //   const [id] = await db("users").insert(user);
-
 //   return findById(id);
-// }
 
-// function remove(id) {
-//   return db("users")
-//     .where({ id })
-//     .del();
-// }
+function remove(id) {
+  return db("users")
+    .where({ id })
+    .del();
+}
 
-// module.exports = {
-//   find,
-//   findBy,
-//   findById,
-//   add,
-//   remove
-// };
+module.exports = {
+  find,
+  findBy,
+  findById,
+  add,
+  remove
+};
