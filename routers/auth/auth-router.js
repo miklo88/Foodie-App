3bc06eb9752b6ff91b9b56a24d3b8a0502dfbb67
Carlos <miklo.concepcion@gmail.com>
2020-02-04 00:53:07 -0600

    route.delete worked on to handle user being deleted. checking functionality and setting up user-model file.

--- routers/auth/auth-router.js
+++ routers/auth/auth-router.js
@@ -13,7 +13,9 @@ router.get("/users", async (req, res, next) => {
 });
 
 // CREATE USER
-router.post("/users", async (req, res, next) => {
+router.post("/users/:id", async (req, res, next) => {
+  const userInfo = req.body;
+
   res.status(200).json({ message: "success POST message", operation: "POST" });
 });
 
@@ -23,8 +25,20 @@ router.put("/users", async (req, res, next) => {
 });
 
 // DELETE USER
-router.delete("/users", async (req, res, next) => {
-  res.status(204);
+router.delete("/users/:id", async (req, res, next) => {
+  const { id } = req.params;
+
+  db.remove("id")
+    .then(deleted => {
+      if (deleted) {
+        res.status(204).end();
+      } else {
+        res.status(404).json({ message: "id not found", operation: "DELETE" });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: "error", err });
+    });
 });
 
 module.exports = router;
