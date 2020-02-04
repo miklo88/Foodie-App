084a60d4727c8f5b57b5db3cd6c71096b9d44f8e
Carlos <miklo.concepcion@gmail.com>
2020-02-04 15:03:53 -0600

    middleware folder created with restricted.js file to handle our token

---
+++ middleware/restricted.js
@@ -0,0 +1,16 @@
+const jwt = require("jsonwebtoken");
+const secrets = require("../config/secrets");
+
+module.exports = () => {
+  return (req, res, next) => {
+    try {
+      const token = req.headers.authorization;
+      const decoded = jwt.verify(token, secrets.jwt);
+
+      req.userId = deoded.subject;
+      next();
+    } catch (err) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+  };
+};
