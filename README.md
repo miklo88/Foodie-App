55ed02d617aaf850a521dcccec6fa21f67d4a9e3
Carlos <miklo.concepcion@gmail.com>
2020-02-04 16:04:14 -0600

    dummy data added in recipes router for front end to play with

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 node.js version Chefs/Foodie Blog. A database where Foodie/Chef users can upload images of their favorite recipes while having the customization down to the ingredients in said recipes.
 
 'https://chefs-view.herokuapp.com/' = homepage endpoint
-'https://chefs-view.herokuapp.com/register' = register - create new user WILL ERROR 404 NOT FOUND
-'https://chefs-view.herokuapp.com/users' = users endpoint // WILL ERROR 401 NOT AUTHORIZED
+'https://chefs-view.herokuapp.com/register' = create new user WILL ERROR 404 NOT FOUND
+'https://chefs-view.herokuapp.com/users' = login users endpoint // WILL ERROR 401 NOT AUTHORIZED
 'https://chefs-view.herokuapp.com/recipes' = recipes endpoint
 
 no data from db yet, just JSON message key value pairs.
--- routers/recipes-router/index.js
+++ routers/recipes-router/index.js
@@ -7,11 +7,52 @@ const db = require("../../database/dbConfig");
 
 router.get("/", async (req, res, next) => {
   try {
-    res.status(200).json({ message: "FOOD RECIPES" });
+    const sortField = req.query.sortby || "id";
+    const recipes = [
+      {
+        id: 1,
+        recipeTitle: "Steak and Eggs",
+        mealType: "breakfast",
+        ingredients: "one hangar steak and two eggs"
+      },
+      {
+        id: 2,
+        recipeTitle: "Southwest Steak Salad",
+        mealType: "lunch",
+        ingredients:
+          "ancho chiles, napa cabbage, corn, lemon, salt, pepper, tortillas, carronts, mayo, hangar steak"
+      },
+      {
+        id: 3,
+        recipeTitle: "Blackbean Veggie Burger",
+        mealType: "Dinner",
+        ingredients:
+          "black beans, quinoa, farro, chickpeas, lettuce, tomato, dijonaise, brioche buns"
+      },
+      {
+        id: 4,
+        recipeTitle: "Carrot Cake Cookies",
+        mealType: "dessert",
+        ingredients:
+          "eggs, carrots, milk, sugar, cream cheese, vanilla, all purpose flour."
+      }
+    ];
+    const response = recipes.sort((a, b) =>
+      a[sortField] < b[sortField] ? -1 : 1
+    );
+    res.status(200).json(response);
   } catch (error) {
     next(error);
   }
 });
+
+// router.get("/", async (req, res, next) => {
+//   try {
+//     res.status(200).json({ message: "FOOD RECIPES" });
+//   } catch (error) {
+//     next(error);
+//   }
+// });
 
 router.get("/:id", async (req, res, next) => {
   try {
