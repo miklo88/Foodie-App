c981cd5e01d537e5f03ac4711108165aa4320f7b
Carlos <miklo.concepcion@gmail.com>
2020-02-05 08:47:09 -0600

    cleaning up endpoints. refactoring CRUD for users and recipes.

--- routers/users-router/index.js
+++ routers/users-router/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 // const restricted = require("../../middleware/restricted");
+
 const userModel = require("../../database/helper_modules/users-model");
 
 const router = express.Router();
@@ -20,7 +21,7 @@ router.get("/:id", async (req, res, next) => {
   try {
     res.status(200).json(user);
   } catch (error) {
-    res.status(418).json({ message: "no no no no - Dikembe Mutombo" });
+    res.status(418).json({ error: "usersRouter err" });
   }
 });
 
