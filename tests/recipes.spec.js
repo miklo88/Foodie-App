e03bf19496fd1baae40e3a5d2e046cd67b6e5714
Carlos <miklo.concepcion@gmail.com>
2020-02-05 16:44:28 -0600

    testing files added to test users and recipes and the connection with the server endpoints and database

---
+++ tests/recipes.spec.js
@@ -0,0 +1,17 @@
+const supertest = require("supertest");
+const server = require("../api/server");
+const db = require("../database/dbConfig");
+
+describe("recipes-router", () => {
+  describe("/recipes endpoint", () => {
+    it("return a status 401", async () => {
+      const res = await supertest(server).get("/recipes");
+      expect(res.status).toBe(401);
+    });
+
+    it("return a body of type JSON", async () => {
+      const res = await supertest(server).get("/recipes");
+      expect(res.type).toBe("application/json");
+    });
+  });
+});
