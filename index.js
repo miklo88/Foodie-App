49868965919490fa0a562c8224d88f423dadf449
Carlos <miklo.concepcion@gmail.com>
2020-02-06 16:45:39 -0600

    tested root api and app is connecting to a client server successfully. going back and updating and deleting a lot of my crud. making code more dry.

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-// heroku deployment. package json is already set up for it.
-// .env in root directory for deploying
 // require("dotenv").config();
-
 const server = require("./api/server");
 
 const host = process.env.HOST || "localhost";
 const port = process.env.PORT || 9000;
 
 // JEST TEST /// if statement to re-direct the server to be exported and ran in the index.spec.js for testing.
-if (!module.parent) {
-  // backend server port/host.
-  server.listen(port, () => {
-    console.log(`\n*** Listening on http://${host}:${port} ***\n`);
-  });
-}
+// if (!module.parent) {
+//   // backend server port/host.
+//   server.listen(port, () => {
+//     console.log(`\n*** Listening on http://${host}:${port} ***\n`);
+//   });
+// }
+// // exporting the api as a server to the index.spec.js file.
+// module.exports = server; ///// JEST TEST
 
-// exporting the api as a server to the index.spec.js file.
-module.exports = server;
+// backend server port/host.
+server.listen(port, () => {
+  console.log(`\n*** Listening on http://${host}:${port} ***\n`);
+});
