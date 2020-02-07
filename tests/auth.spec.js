981b1956f8ae0ccf021b36d5b27547708f5f9f6b
Carlos <miklo.concepcion@gmail.com>
2020-02-06 19:22:37 -0600

    salting some more test files in my app. focus of testing for this app is the flow of data. from database to server to client. the endpoints get/post/put/delete are the focus and how the data flow is channeled. aka just making sure client is getting the data from the database they are asking for. and being able to manipulate the database.

---
+++ tests/auth.spec.js
@@ -0,0 +1,17 @@
+// const supertest = require("supertest");
+// const server = require("../api/server");
+// const db = require("../database/dbConfig");
+
+// describe("/auth/register-router", () => {
+//   describe("/auth/register endpoint", () => {
+//     it("return a status 200", async () => {
+//       const res = await supertest(server).get("/auth/register");
+//       expect(res.status).toBe(200);
+//     });
+
+//     it("return a body of type JSON", async () => {
+//       const res = await supertest(server).get("/auth/register");
+//       expect(res.type).toBe("application/json");
+//     });
+//   });
+// });
---
+++ tests/users.spec.js
@@ -0,0 +1,16 @@
+// // actual integration test SUPERTEST library for endpoints.
+// const supertest = require("supertest");
+// // server is the port on index.js
+// const server = require("../users");
+
+// test("users route", async () => {
+//   const res = await supertest(server).get("/");
+//   //   doest it return the expected status codes?
+//   expect(res.status).toBe(200);
+
+//   //   doest it return the expected data format?
+//   expect(res.type).toBe("application/json");
+
+//   //   doest it return the expected data?
+//   expect(res.body.message).toBe(users);
+// });
